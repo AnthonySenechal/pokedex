d25769ae19d403b24c87cca2b10a272a2857593c
Anthony Senechal <anthony.senechal59128@gmail.com>
2023-03-29 16:14:50 +0200

    state.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 
 import './App.css'
 import PokemonCard from './components/PokemonCard'
+import { useState } from "react";
 
 function App() {
-  const pokemon = pokemonList[0];
+  let [pokemonIndex, setPokemonIndex] = useState (0);
+    
+  function handleClick () {
+    setPokemonIndex(pokemonIndex + 1)}
+    let pokemon = {
+      name: pokemonList[pokemonIndex].name,
+      imgSrc: pokemonList[pokemonIndex].imgSrc,
+    }
+
   return (
     <div className="App">
       <PokemonCard 
       name = {pokemon.name}
       imgSrc = {pokemon.imgSrc}
       />
+       <button onClick={handleClick}>next</button>
+        
 
       
     </div>
@@ -23,9 +34,25 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
+
 
 export default App
 
