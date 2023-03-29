6c729f007b23ada78aa39c40c0acd92f93bb5f14
Anthony Senechal <anthony.senechal59128@gmail.com>
2023-03-29 10:25:36 +0200

    props.1

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,20 +1,8 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-function PokemonCard () {
-    const pokemon = pokemonList[0];
+function PokemonCard (props) {
     return (
     <figure className="card">
-        {pokemon.imgSrc ? (
-            <img src={pokemon.imgSrc} alt={pokemon.name}/>
+        {props.imgSrc ? (
+            <img src={props.imgSrc} alt={props.name}/>
         ) : (
             <p>???</p>
         )
